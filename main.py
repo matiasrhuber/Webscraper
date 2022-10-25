607520abec128e5fc0abda3e113c68cb82349ee3
matiasrhuber <matiasrhuber@gmail.com>
2022-10-24 22:09:18 -0300

    Initial model, opening web

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 #%%
 import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
 
-print("hello")
+# Setup of webdriver
+path = os.getcwd()
+driver = webdriver.Safari(executable_path = '/usr/bin/safaridriver')
+# 
+products = []
+prices = []
+ratings = []
+driver.get("https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2")
+# Extraction and parsing of page content
+content = driver.page_source
+print("----------------------------------------------------\ncontent:")
+print(content)
+soup = BeautifulSoup(content, features='lxml')
+print("----------------------------------------------------\nsoup:")
+print(soup)
+print("----------------------------------------------------\nsoup_list:")
+print(soup.find_all('a',href=True, attrs={'class':'_1fQZEK'}))
+# for a in 
+
+driver.quit()
 # %%
